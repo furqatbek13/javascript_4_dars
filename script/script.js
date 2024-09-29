39bcce841bd30e22c335261645d3b2289b2dafa2
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-29 22:55:02 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -50,4 +50,91 @@
 
 
 // 2-misol
+// let a = Math.floor(prompt("a ning qiymatini kiriting: ")); 
+// let b = Math.floor(prompt("b ning qiymatini kiriting: ")); 
+// let natija = 0;
+// for (let i = b - 1; i > a; i--) {
+//     console.log(i); 
+//     natija++; 
+// }
+// console.log("Chiqarilgan sonlar soni: " + natija);
+
+
+// 3-misol
+// let narx = prompt("konfetni narxini kiriting: ");
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i + " kg konfet narxi: " + (i * narx) + " so'm");
+// }
+
+
+//  4-misol
+// let narx = prompt("konfetni narxini kiriting: ");
+// for (var i = 1.2; i <= 2.0; i += 0.2) {
+//     console.log(i.toFixed(1) + " kg konfet narxi: " + (i * narx).toFixed(2) + " so'm");
+// }
+
+
+//   5-misol
+// let a = Math.floor(prompt("a ning qiymatini kiriting: "));
+// let b = Math.floor(prompt("b ning qiymatini kiriting: "));
+// let yigindi = 0;
+// for (let i = a; i <= b; i++) {
+//     yigindi += i;  
+// }
+// console.log("Yig'indisi: " + yigindi);
+
+
+//  6-misol
+// let a = Math.floor(prompt("a ning qiymatini kiriting: "));
+// let b = Math.floor(prompt("b ning qiymatini kiriting: "));
+// let kopaytma = 1;
+// for (let i = a; i <= b; i++) {
+//     kopaytma *= i; 
+// }
+// console.log("Ko'paytma: " + kopaytma);
+
+//  7-misol
+// let a = Math.floor(prompt("a ning qiymatini kiriting: "));
+// let b = Math.floor(prompt("b ning qiymatini kiriting: "));
+// let yigindi = 0;
+// for (let i = a; i <= b; i++) {
+//     yigindi += i * i;  
+// }
+// console.log("Kvadratlar yig'indisi: " + yigindi);
+
+
+//  8-misol
+// let n = Math.floor(prompt("n ning iqymatini kiriting: "));  
+// let S = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     S += 1 / i; 
+// }
+// console.log("Yig'indi S: " + S);
+
+
+//   9-misol
+// let n = prompt("n ning iqymatini kiriting: ");  
+// let S = 1;  
+
+// for (let i = 1.1; i <= n; i += 0.1) {
+//     S *= i;  
+// }
+// console.log("Ko'paytma S: " + S);
+
+
+//   10-misol
+// let n = prompt("n ning qiymatini kriting: "); 
+// let S = 0;  
+
+// for (let i = 1; i <= n; i++) {
+//     let qoshiluvchi = 2 * i - 1;  
+//     S += qoshiluvchi; 
+//     console.log("Qo'shiluvchi: " + qoshiluvchi + ", Jami yig'indi: " + S); 
+// }
+// console.log("Natijada " + n + " ning kvadrati: " + S);
+
+
+//   11-misol
+
 
