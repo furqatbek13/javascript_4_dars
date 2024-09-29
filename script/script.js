d850789c97df79fc644623a75d518f334754d217
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-29 22:26:31 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// UYGA VAZIFA ///
+// Darsdagi misollar ///
 // 1-misol
 // let k = Math.floor(prompt("K ning qiymatini kiriting: "));
 // let n = Math.floor(prompt("N ning qiymatini kiriting: "));
@@ -16,3 +16,38 @@
 // }
 
 // 3-misol
+// let n = Math.floor(prompt("n ga qiymat kiriting: "));
+// let S = 0;
+// for (let i = n; i <= 2 * n; i++) {
+//     S += i * i;  
+// }
+// console.log("Yig'indi S: " + S);
+
+
+
+//  4-misol
+// let N = Math.floor(prompt("N ning qiymatini kiriting: "));  
+// let S = 0;
+// for (let i = 1; i <= N; i++) {
+//     S += Math.pow(i, N - i + 1);
+// }
+// console.log("Yig'indi S: " + S);
+
+
+
+
+
+/// UYGA VAZIFA  ///
+// 1-misol
+// let a = Math.floor(prompt("a ning qiymatini kiriting; "));  
+// let b = Math.floor(prompt("b ning qiymatini kiriting; "));  
+// let count = 0;
+// for (let i = a; i <= b; i++) {
+//     console.log(i); 
+//     count++; 
+// }
+// console.log("Chiqarilgan sonlar soni: " + count);
+
+
+// 2-misol
+
