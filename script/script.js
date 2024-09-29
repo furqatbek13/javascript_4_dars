e1ec6f0bf40fd9dee4fd32d4735317e0d8b6b020
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-29 22:12:23 +0500

    Update script.js

---
+++ script/script.js
@@ -0,0 +1,18 @@
+// UYGA VAZIFA ///
+// 1-misol
+// let k = Math.floor(prompt("K ning qiymatini kiriting: "));
+// let n = Math.floor(prompt("N ning qiymatini kiriting: "));
+// for (var i = 0; i < n; i++) {
+//     console.log(k);
+// }
+
+
+
+//  2-misol
+// let narx = prompt("1 kg konfetni narxini kiriting: ");
+// for (let i = 1; i <= 10; i++) {
+//     let kilogramm = i / 10;
+//     console.log(kilogramm + " kg konfet narxi: " + (kilogramm * narx) + " so'm");
+// }
+
+// 3-misol
